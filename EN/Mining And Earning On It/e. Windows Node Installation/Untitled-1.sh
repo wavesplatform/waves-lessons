c90ad57236b96c514dd0881fb1931767ad353c30
Pavel Prusakov <pavelprusakov@Pavels-MacBook-Pro.local>
2022-07-20 09:53:12 -0300

    interim node installation ubuntu

--- EN/Mining And Earning On It/e. Windows Node Installation/Untitled-1.sh
+++ EN/Mining And Earning On It/e. Windows Node Installation/Untitled-1.sh
@@ -44,4 +44,93 @@ docker run -d --name my-waves-node \
 -e WAVES_WALLET_PASSWORD=Lancelot77_ \
 wavesplatform/wavesnode:latest
 
+
+    docker run -d \
+     --name my-waves-node \
+     -p 6869:6869 \
+     --platform linux/amd64 \
+     -e WAVES_WALLET_SEED="K6XzUChB6DwTYCM1WxtVrv1BM6jTdcaBJrn6vkB3cK7qXCnqLV2Jj1UgdEzr2dF98PChALCzbkF3Ni1cBFKanXErzuBdJaeFc2etFDBLjdov47qsfTx5bHxx5Uv" \
+     -e WAVES_WALLET_PASSWORD="Lancelot77)" \
+     wavesplatform/wavesnode:latest
+
 http://127.0.0.1:6869/
+
+-----
+
+waves {
+
+  wallet {
+    # Password to protect wallet file
+    password="Lancelot77_!"
+
+    # Wallet seed as BASE58 string
+    seed="R2nnbazCsUn1ihTC5mhTNSe1UZ2bNmyn966HV76rWQHKBWpZpwnSDj155aon7c6WTdtHZpdabL9p6ke1aR2c9SiwBKb6SsfYKrGktzbT3U8QQnftZ1MK8F4wB5eNgL7"
+  }
+
+  blockchain {
+    # Blockchain type: TESTNET | MAINNET | STAGENET | CUSTOM
+    type = MAINNET
+  }
+
+  rest-api {
+    # Enable/disable REST API
+    enable = yes
+
+    # Network address to bind to
+    bind-address = "127.0.0.1"
+
+    # Port to listen to REST API requests
+    port = 6869
+
+    # Hash of API key string
+    api-key-hash = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzaWciOiIzUW5lV2pUd2Y0WjJhYzdRTWlDRmtUNll4cGVMQWFQTnB6TFVta0xBZThNYXdBNUJzM21tcjYxZDllTllUUFRNMVZXUEpyRDF4VkZNNXllZlRkUzJrYXhBIiwiYSI6IjNQM203QU50TmV6Wk1kQ2NqMWRkNFNScUpMUFdLcEJpWDZ5IiwibmIiOiJXIiwidXNlcl9uYW1lIjoiN1ZxWDhkalFGYktrTXd4bXUxWVcxVlU2Zkd2ZEtjTXp6N013VGlIa1pxQlAiLCJzY29wZSI6WyJnZW5lcmFsIl0sImx0Ijo4NjQwMCwicGsiOiI3VnFYOGRqUUZiS2tNd3htdTFZVzFWVTZmR3ZkS2NNeno3TXdUaUhrWnFCUCIsImV4cCI6MTY1ODM5Mzg5NCwiZXhwMCI6MTY1ODMyMTUzMywianRpIjoiZmUyYzZmNmYtZTA0My00ZTEyLWJmODQtM2I3NWNmYjY3NWJiIiwiY2lkIjoid2F2ZXMuZXhjaGFuZ2UifQ.Yi6K6TtV_Zz4g6vgc0vBoIh1TNfeILgl9qDBBft5DYUqVxIOY-Yijx7JD4K9W7rt2hifmZI9v4JRtSDA0o8Na-SGYLuQ2sD3VH2tTqPvX3NDpLoJ3y9PdtZ618knzwE7TGkRErL20pa7O0Z6NVfHxhAZDiM_KUuFT5d271MHCOXustMvCFvpn4tuROc6T00Wik71baoxaCqWJTSuwx1-wKhRdqsGT2HI_4QGYQYYBbw2a9sdXYXIYnT8pkJ0cNqtIvLCVjwJqEYjMThH47Y-xZLBMCfxxMD0Pj2mvBG8YSUy3A6oBzgMq5c7XH3kiaytLwbYU86LxSD3If4Cm3uBNg"
+  }
+
+  network {
+    # Node name to send during handshake. Comment this string out to set random node name.
+    node-name = "waves-test-default-node-name"
+  }
+
+  # Uncomment to enable the gRPC extension
+  # extensions += com.wavesplatform.api.grpc.GRPCServerExtension
+}
+
+---
+
+waves {
+
+  wallet {
+    # Password to protect wallet file
+    password="Lancelot77_"
+
+    # Wallet seed as BASE58 string
+    seed="R2nnbazCsUn1ihTC5mhTNSe1UZ2bNmyn966HV76rWQHKBWpZpwnSDj155aon7c6WTdtHZpdabL9p6ke1aR2c9SiwBKb6SsfYKrGktzbT3U8QQnftZ1MK8F4wB5eNgL7"
+  }
+
+  blockchain {
+    # Blockchain type: TESTNET | MAINNET | STAGENET | CUSTOM
+    type = MAINNET
+  }
+
+  rest-api {
+    # Enable/disable REST API
+    enable = yes
+
+    # Network address to bind to
+    bind-address = "127.0.0.1"
+
+    # Port to listen to REST API requests
+    port = 6869
+
+    # Hash of API key string
+    api-key-hash = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzaWciOiIzUW5lV2pUd2Y0WjJhYzdRTWlDRmtUNll4cGVMQWFQTnB6TFVta0xBZThNYXdBNUJzM21tcjYxZDllTllUUFRNMVZXUEpyRDF4VkZNNXllZlRkUzJrYXhBIiwiYSI6IjNQM203QU50TmV6Wk1kQ2NqMWRkNFNScUpMUFdLcEJpWDZ5IiwibmIiOiJXIiwidXNlcl9uYW1lIjoiN1ZxWDhkalFGYktrTXd4bXUxWVcxVlU2Zkd2ZEtjTXp6N013VGlIa1pxQlAiLCJzY29wZSI6WyJnZW5lcmFsIl0sImx0Ijo4NjQwMCwicGsiOiI3VnFYOGRqUUZiS2tNd3htdTFZVzFWVTZmR3ZkS2NNeno3TXdUaUhrWnFCUCIsImV4cCI6MTY1ODM5Mzg5NCwiZXhwMCI6MTY1ODMyMTUzMywianRpIjoiZmUyYzZmNmYtZTA0My00ZTEyLWJmODQtM2I3NWNmYjY3NWJiIiwiY2lkIjoid2F2ZXMuZXhjaGFuZ2UifQ.Yi6K6TtV_Zz4g6vgc0vBoIh1TNfeILgl9qDBBft5DYUqVxIOY-Yijx7JD4K9W7rt2hifmZI9v4JRtSDA0o8Na-SGYLuQ2sD3VH2tTqPvX3NDpLoJ3y9PdtZ618knzwE7TGkRErL20pa7O0Z6NVfHxhAZDiM_KUuFT5d271MHCOXustMvCFvpn4tuROc6T00Wik71baoxaCqWJTSuwx1-wKhRdqsGT2HI_4QGYQYYBbw2a9sdXYXIYnT8pkJ0cNqtIvLCVjwJqEYjMThH47Y-xZLBMCfxxMD0Pj2mvBG8YSUy3A6oBzgMq5c7XH3kiaytLwbYU86LxSD3If4Cm3uBNg"
+  }
+
+  network {
+    # Node name to send during handshake. Comment this string out to set random node name.
+    node-name = "waves-test-default-node-name"
+  }
+
+    # Uncomment to enable the gRPC extension
+    # extensions += com.wavesplatform.api.grpc.GRPCServerExtension
+}
