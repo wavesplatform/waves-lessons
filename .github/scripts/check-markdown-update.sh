48e6dc3af016a51b79be33772668e51ce86cf054
Alexandr Devyatkin <adevyatkin@web3tech.ru>
2022-07-29 17:03:07 +0300

    - markdown update

--- .github/scripts/check-markdown-update.sh
+++ .github/scripts/check-markdown-update.sh
@@ -1,10 +1,9 @@
-RESPONSE=$(curl -X 'GET' 'https://api-dev.waves.tech/api/v1/test' \
+RESPONSE=$(curl -X 'GET' "https://{$1}/sync" \
   -H 'accept: */*' \
-  -H "secret: $1")
-STATUSCODE=$(jq '.status' <<< "$RESPONSE")
+  -H "secret: $2")
+STATUSCODE=$(jq '.status' <<<"$RESPONSE")
 echo "response: $RESPONSE"
 echo "status code: $STATUSCODE"
 if test "$STATUSCODE" -ne 200; then
   exit 1
 fi
-
