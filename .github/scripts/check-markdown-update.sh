cbf31a64512efceccfbd3516715101835bd3c079
Roman Dolgov <321677+alter@users.noreply.github.com>
2022-09-01 11:22:56 +0400

    Update check-markdown-update.sh
    set connection timeout to 3s

--- .github/scripts/check-markdown-update.sh
+++ .github/scripts/check-markdown-update.sh
@@ -1,8 +1,10 @@
 URL="https://$1/sync"
 echo "sending GET $URL"
-RESPONSE=$(curl --max-time 180 -X 'GET' "$URL" \
+date
+RESPONSE=$(curl --connection-timeout 3 --max-time 180 -X 'GET' "$URL" \
   -H 'accept: */*' \
   -H "secret: $2")
+date
 STATUS=$(jq -r '.status' <<<"$RESPONSE")
 echo "status: $STATUS"
 echo "response: $RESPONSE"
