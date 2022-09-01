3070573e39cf725c0caebf29b48b7759e4920523
Roman Dolgov <321677+alter@users.noreply.github.com>
2022-09-01 11:33:04 +0400

    Update check-markdown-update.sh

--- .github/scripts/check-markdown-update.sh
+++ .github/scripts/check-markdown-update.sh
@@ -1,9 +1,10 @@
 URL="https://$1/sync"
 echo "sending GET $URL"
-exit -1
+date
 RESPONSE=$(curl --connect-timeout 5 --no-keepalive -X 'GET' "$URL" \
   -H 'accept: */*' \
   -H "secret: $2")
+date
 STATUS=$(jq -r '.status' <<<"$RESPONSE")
 echo "status: $STATUS"
 echo "response: $RESPONSE"
