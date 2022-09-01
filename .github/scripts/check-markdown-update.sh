ed74820202e1083e3e1266360a4b1f634ec65dfa
Roman Dolgov <321677+alter@users.noreply.github.com>
2022-09-01 11:27:01 +0400

    Update check-markdown-update.sh
    update curl options, disable keepalive

--- .github/scripts/check-markdown-update.sh
+++ .github/scripts/check-markdown-update.sh
@@ -1,11 +1,8 @@
 URL="https://$1/sync"
 echo "sending GET $URL"
-date
-curl --version
-RESPONSE=$(curl --connect-timeout 3 --max-time 180 -X 'GET' "$URL" \
+RESPONSE=$(curl --connect-timeout 5 --no-keepalive -X 'GET' "$URL" \
   -H 'accept: */*' \
   -H "secret: $2")
-date
 STATUS=$(jq -r '.status' <<<"$RESPONSE")
 echo "status: $STATUS"
 echo "response: $RESPONSE"
